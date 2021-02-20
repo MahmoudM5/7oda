6917c48cb9626f16979fb1c02a171c176b520191
MahmoudM5 <77855840+MahmoudM5@users.noreply.github.com>
2021-02-20 04:05:54 +0200

    Add files via upload

---
+++ File_Bot/7oda.lua
@@ -0,0 +1,32 @@
+local function y(msg)
+local text = msg.content_.text_
+if not database:get(bot_id..'y:Status'..msg.chat_id_) then
+
+
+if text then 
+list = {'موون'}
+for k,v in pairs(list) do
+if string.find(text,v) ~= nil then
+send(msg.chat_id_,msg.id_, 'وعليكم السلام 💖') 
+return false
+end
+end
+end
+
+end
+
+if text == 'تفعيل ردود البوت' and Manager(msg) then
+database:del(bot_id..'repdark:Status'..msg.chat_id_)
+send(msg.chat_id_, msg.id_,'◍ تم تفعيل الردود بنجاح')
+return false
+end
+
+if text == 'تعطيل ردود البوت' and Manager(msg) then
+database:set(bot_id..'repdark:Status'..msg.chat_id_,true)
+send(msg.chat_id_, msg.id_,'◍ تم تعطيل الردود بنجاح')
+return false
+end
+end
+return {
+Poyka = y
+}
